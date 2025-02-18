4225ab44485ff7e112ffe99d8e2a3be8c31236b8
Blazzzeee <legendgamer1007@gmail.com>
2025-02-18 16:36:05 +0530

    install script ready

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 
 # Move network_manager to ~/.config/
-echo -e "\033[1;34mMoving network_manager to ~/.config/\033[0m"
-mv -r networkmanager/ ~/.config/
+echo -e "\033[1;34mMoving networkmanager to ~/.config/\033[0m"
+cp -r networkmanager/ ~/.config/
 if [ $? -eq 0 ]; then
     echo -e "\033[1;32mSuccess: network_manager moved to ~/.config/\033[0m"
 else
@@ -21,16 +21,16 @@ fi
 
 # Move rofi to ~/.config/rofi/wifi/
 echo -e "\033[1;34mMoving rofi to ~/.config/rofi/wifi/\033[0m"
-mv -r rofi/ ~/.config/rofi/wifi/
+cp -r rofi/ ~/.config/rofi/wifi/
 if [ $? -eq 0 ]; then
     echo -e "\033[1;32mSuccess: rofi moved to ~/.config/rofi/wifi/\033[0m"
 else
     echo -e "\033[1;31mFailure: Could not move rofi to ~/.config/rofi/wifi/\033[0m"
 fi
 
-# Move network_manager to /usr/local/bin/
+# Move network_manager script to /usr/local/bin/
 echo -e "\033[1;34mMoving network_manager to /usr/local/bin/\033[0m"
-sudo mv network_manager /usr/local/bin/
+sudo cp network_manager /usr/local/bin/
 if [ $? -eq 0 ]; then
     echo -e "\033[1;32mSuccess: network_manager moved to /usr/local/bin/\033[0m"
 else
