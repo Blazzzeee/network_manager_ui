cbef6153bf67939a71532299bf94d0af662631f8
Blazzzeee <legendgamer1007@gmail.com>
2026-01-27 22:27:22 +0530

    remove pygobject stubs

--- nix/default.nix
+++ nix/default.nix
@@ -17,19 +17,6 @@
   writeText
 }:
 let
-  pygobject-stubs' = python3Packages.pygobject-stubs.overridePythonAttrs (old: {
-    buildInputs = (old.buildInputs or [ ]) ++ ([
-      networkmanager
-      python3Packages.pygobject3
-    ]);
-    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ gobject-introspection ];
-    # see https://github.com/pygobject/pygobject-stubs/pull/204
-    patches = (old.patches or [ ]) ++ [ ./pygobject.patch ];
-    postPatch = ''
-      python tools/generate.py NM 1.0 > src/gi-stubs/repository/NM.pyi
-    '';
-  });
-
   defaultConfig = writeText "config.ini" ''
     [dmenu]
     dmenu_command = rofi -dmenu -theme ~/.config/rofi/wifi/config.rasi -i -no-history -matching fuzzy -no-tokenize -hover-select
@@ -55,7 +42,6 @@ python3Packages.buildPythonPackage {
   ];
 
   nativeBuildInputs = [
-    pygobject-stubs'
     python3Packages.mypy
     ruff
     gobject-introspection
